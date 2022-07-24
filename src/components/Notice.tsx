3963d644166c63368581ea0364591a1c62caa282
k89jy <k89jy@naver.com>
2022-07-25 02:48:15 +0900

    add warning popups and link to warning page

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-const Button = styled.button`
-    color: red;
-    border: 2px solod white;
-`;
 
 const Warning = styled.img`
     width: 100%;
