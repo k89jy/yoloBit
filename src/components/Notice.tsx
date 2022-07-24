8aae788520d1b501e62c3f849c5bafa6023df39a
k89jy <k89jy@naver.com>
2022-07-25 02:04:58 +0900

    add warning popups and link to warning page

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import { Link } from 'react-router-dom';
 const Button = styled.button`
     color: red;
     border: 2px solod white;
 `;
 
+const Warning = styled.img`
+    width: 100%;
+`;
+const WarningWrapper = styled.section`
+    position: fixed;
+    width: 400px;
+    top: 50%;
+    left: 50%;
+    margin: -240px 0 0 -200px;
+    padding: 0;
+    background: #fff;
+    border: 0;
+    display: block;
+`;
+const WarningLink = styled(Link)``;
+
 function Notice(): JSX.Element {
-    function alert() {
-        window.alert('hello world');
-    }
+    const upBitWarningUrl = '/service_center/notice?id=2677';
+
     return (
         <>
-            <Button onClick={() => alert()}></Button>
+            <WarningWrapper>
+                <WarningLink to={upBitWarningUrl}>
+                    <Warning src="https://static.upbit.com/guide/risk_warning_pc.png" />
+                </WarningLink>
+            </WarningWrapper>
         </>
     );
 }
