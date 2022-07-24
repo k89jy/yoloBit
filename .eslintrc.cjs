719d61778af41ec5d64d4dc49d46a7a83b3abc4f
k89jy <k89jy@naver.com>
2022-07-24 19:38:39 +0900

    testing modal module && reconfigure linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,22 @@ module.exports = {
       project: ['./tsconfig.json'],
     },
     plugins: ['@typescript-eslint'],
+    overrides: [
+      {
+        files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+  
+        // As mentioned in the comments, you should extend TypeScript plugins here,
+        // instead of extending them outside the `overrides`.
+        // If you don't want to extend any rules, you don't need an `extends` attribute.
+        extends: [
+          'plugin:@typescript-eslint/recommended',
+          'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        ],
     extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      'plugin:prettier/recommended',
-      'prettier/@typescript-eslint'
+      // 'eslint:recommended',
+      // 'plugin:@typescript-eslint/recommended',
+      // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      // 'plugin:prettier/recommended',
+      // 'prettier/@typescript-eslint'
     ],
   };
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React, { useState } from 'react';
+import ReactModal from 'react-modal';
 
-function App() {
-    return <></>;
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+function App(): JSX.Element {
+    let subtitle: { style: { color: string } };
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+    return (
+        <ReactModal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+        />
+    );
 }
 
 export default App;
